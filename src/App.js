a58dd54ee0c6b77654baee92ea581509eb06d213
Yannis <yannis.bouacida@appliedtechnology.se>
2020-07-09 17:02:29 +0200

    started get transaction function

--- src/App.js
+++ src/App.js
@@ -1,31 +1,41 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import { ethers } from "ethers";
-import Web3 from "web3";
+const provider = new ethers.providers.Web3Provider(window.ethereum);
 
 function App() {
   const [ethBalance, setEthBalance] = useState([]);
 
+  const getAdress = () => {
+    return provider.provider.selectedAddress;
+  };
+
   const loadWeb3 = async () => {
     if (window.ethereum) {
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
       await window.ethereum.enable();
-      let adress = provider.provider.selectedAddress;
+      let adress = getAdress();
       let balance = await provider.getBalance(adress);
-      // console.log(provider.getTransactionCount(adress));
       return setEthBalance(ethers.utils.formatEther(balance));
     } else {
       setEthBalance(`Please install metamask`);
     }
   };
 
+  const getTransactions = async () => {
+    let provider = new ethers.providers.EtherscanProvider();
+    let history = await provider.getHistory(getAdress());
+    return console.log(history.filter(history => history.from === '0x3Ef3e95e5025D89966695DA57c3ed031eeBC1bd0'));
+  };
+
   useEffect(() => {
     loadWeb3();
+    getTransactions();
   });
+
   return (
     <div className="App">
       <h2>Bonsai</h2>
-      <h3>{ethBalance}</h3>
+      <h3>Ethereum balance: {ethBalance}</h3>
     </div>
   );
 }
