c78a608beaa601e8dcf2bb87d8d45e03c0deaa08
Yannis <yannis.bouacida@appliedtechnology.se>
2020-07-09 18:35:45 +0200

    hardcoded transaction and getting transactions

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ const provider = new ethers.providers.Web3Provider(window.ethereum);
 
 function App() {
   const [ethBalance, setEthBalance] = useState([]);
+  const [transactionHistory, setTransactionHistory] = useState([]);
 
   const getAdress = () => {
     return provider.provider.selectedAddress;
@@ -13,18 +14,20 @@ function App() {
   const loadWeb3 = async () => {
     if (window.ethereum) {
       await window.ethereum.enable();
-      let adress = getAdress();
-      let balance = await provider.getBalance(adress);
+      const adress = getAdress();
+      const balance = await provider.getBalance(adress);
       return setEthBalance(ethers.utils.formatEther(balance));
     } else {
       setEthBalance(`Please install metamask`);
     }
   };
 
+
   const getTransactions = async () => {
-    let provider = new ethers.providers.EtherscanProvider();
-    let history = await provider.getHistory(getAdress());
-    return console.log(history.filter(history => history.from === '0x3Ef3e95e5025D89966695DA57c3ed031eeBC1bd0'));
+    const provider = new ethers.providers.EtherscanProvider();
+    const adress = getAdress();
+    const history = await provider.getHistory(adress);
+    return setTransactionHistory(history);
   };
 
   useEffect(() => {
@@ -36,6 +39,18 @@ function App() {
     <div className="App">
       <h2>Bonsai</h2>
       <h3>Ethereum balance: {ethBalance}</h3>
+      {transactionHistory.map((transaction, key) => (
+        <li key={transaction.hash}>
+          <a href={`https://etherscan.io/tx/${transaction.hash}`}>
+            {transaction.hash}
+          </a>
+          <p>
+            {transaction.from === "0x3Ef3e95e5025D89966695DA57c3ed031eeBC1bd0"
+              ? "sent"
+              : "received"}
+          </p>
+        </li>
+      ))}
     </div>
   );
 }
